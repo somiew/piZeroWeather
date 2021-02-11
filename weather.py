f57b0f81bf200460006f530d9ed3fa1d3f45b6f3
somiew <somiew@hotmail.com>
2021-02-11 11:14:18 +0100

    moved list creation from outside for-loop

--- weather.py
+++ weather.py
@@ -11,16 +11,18 @@ while loop:
 
     first_day = soup.find_all(class_="row main-display")
 
-    # create list, find all top temps and append to list
+    # create empty list
+    temp = []
+
+    # find all top temps and append to list
     for i in first_day:
-        temp = []
         temp_list = i.find(class_='top_temp')
         temp.append(temp_list.text)
 
     # fourletterphat-code
     flp.clear()
-    flp.print_str(temp[0] + '°C')
+    flp.print_str(temp[0] + ' C')    # degree sign not available in flp unfortunatly
     flp.show()
     
-    #print(temp[0] + '°C')
+    print(temp[0] + '°C')    # print to console, because why not
     time.sleep(60)
